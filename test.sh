0b96d2eb70e3e980c262ab23bc258775da2d610a
Gabor Torok <gtorok@babbel.com>
2018-06-24 15:43:31 +0200

    Run tests on a copy of the database

--- test.sh
+++ test.sh
@@ -3,6 +3,10 @@
 SERVICE_URL=http://localhost:3000
 C='\033[0;33m'
 NC='\033[0m'
+TEST_DB=./data/books.json
+BACKUP_DB=${TEST_DB}.backup
+
+cp $TEST_DB $BACKUP_DB
 
 echo -e "${C}Getting all the books${NC}"
 curl -X GET $SERVICE_URL/books
@@ -19,3 +23,6 @@ echo
 echo -e "${C}Updating new book${NC}"
 curl -X PUT -d "bookshelf=3" $SERVICE_URL/books/2
 echo
+
+mv $BACKUP_DB $TEST_DB
+
